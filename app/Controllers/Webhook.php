251465dc43eb377957b72db75fe65f8f2c866081
radityohanif <sayaanakit80@gmail.com>
2021-12-28 22:51:40 +0700

    hello message

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -17,13 +17,16 @@ class Webhook extends BaseController
     protected $httpClient;
     protected $bot;
 
+    protected $request;
+    protected $replyToken;
+
     public function __construct()
     {
         // Sesuaikan dengan identitas chatbot
         $this->channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
         $this->channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
 
-        $this->pass_signature = true;
+        $this->pass_signature = false;
         $this->httpClient = new CurlHTTPClient($this->channel_access_token);
         $this->bot = new LINEBot($this->httpClient, ['channelSecret' => $this->channel_secret]);
     }
@@ -51,11 +54,23 @@ class Webhook extends BaseController
         }
 
         // Chatbot code start from here.. 👇👇
-        $this->hello();
+        // Fetch request data
+        $this->request = json_decode($body, true);
+        foreach ($this->request['events'] as $event) {
+            if ($event['type'] == 'message') {
+                if ($event['message']['type'] == 'text') {
+                    $this->replyToken = $event['replyToken'];
+                    $this->hello();
+                }
+            }
+        }
     }
 
     public function hello()
     {
-        return "Hello, Saya Bot";
+        $this->bot->replyText(
+            $this->replyToken,
+            'Hello selamat datang di warung pecel lele seger roso'
+        );
     }
 }
