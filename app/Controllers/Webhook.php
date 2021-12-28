92a14c7eebbab20e22305b5ab75ad929786fdfec
radityohanif <sayaanakit80@gmail.com>
2021-12-28 22:41:28 +0700

    ubah pass_signature

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -23,7 +23,7 @@ class Webhook extends BaseController
         $this->channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
         $this->channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
 
-        $this->pass_signature = false;
+        $this->pass_signature = true;
         $this->httpClient = new CurlHTTPClient($this->channel_access_token);
         $this->bot = new LINEBot($this->httpClient, ['channelSecret' => $this->channel_secret]);
     }
