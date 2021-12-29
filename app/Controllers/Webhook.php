c6fc481f89410f403e602d573760f18feb7d1611
radityohanif <sayaanakit80@gmail.com>
2021-12-29 20:52:41 +0700

    tes

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -4,19 +4,21 @@ namespace App\Controllers;
 
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class Webhook extends BaseController
 {
     protected $replyToken;
-    protected $lineBot;
+    protected $bot;
 
     public function __construct()
     {
-        // Initial chatlineBot model
+        // Initial chatbot model
         $channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
         $channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
         $httpClient = new CurlHTTPClient($channel_access_token);
-        $this->lineBot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+        $this->bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
     }
 
     public function index()
@@ -31,18 +33,17 @@ class Webhook extends BaseController
         foreach ($this->request['events'] as $event) {
             if ($event['type'] == 'message') {
                 if ($event['message']['type'] == 'text') {
-                    $this->replyToken = $event['replyToken'];   // set replyToken
-                    $this->salam();
+                    $this->replyToken = $event['replyToken'];
+                    $this->test();
                 }
             }
         }
     }
 
-    public function salam()
+    public function test()
     {
-        $this->lineBot->replyText(
-            $this->replyToken,
-            "Halo selamat datang di warung pecel lele maju roso \nkamu mau pesan apa hari ini ?"
-        );
+        $jawaban = new MultiMessageBuilder();
+        $jawaban->add(new TextMessageBuilder('Halo Selamat datang di warung pecel lele maju roso 😀'));
+        $jawaban->add(new TextMessageBuilder('Kamu mau pesan apa..'));
     }
 }
