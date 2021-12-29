446a157ae9e8d9d8baa70d9f0ad6b27d4b55c03e
radityohanif <sayaanakit80@gmail.com>
2021-12-29 16:37:09 +0700

    redirect

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -32,7 +32,7 @@ class Webhook extends BaseController
             if ($event['type'] == 'message') {
                 if ($event['message']['type'] == 'text') {
                     $this->replyToken = $event['replyToken'];   // set replyToken
-                    redirect()->to('Salam/kirimPesan');
+                    redirect('Salam/kirimPesan');
                 }
             }
         }
