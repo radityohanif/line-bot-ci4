970255f384097228bea4a4d782109663409c0353
radityohanif <sayaanakit80@gmail.com>
2021-12-29 20:08:34 +0700

    tes

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -2,8 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Model\Salam;
-use App\Model\TerimaKasih;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
@@ -11,14 +9,9 @@ class Webhook extends BaseController
 {
     protected $replyToken;
     protected $lineBot;
-    protected $salamModel;
-    protected $terimaKasihModel;
 
     public function __construct()
     {
-        $this->salamModel = new Salam();
-        $this->terimaKasihModel = new TerimaKasih();
-
         // Initial chatlineBot model
         $channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
         $channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
@@ -39,12 +32,6 @@ class Webhook extends BaseController
             if ($event['type'] == 'message') {
                 if ($event['message']['type'] == 'text') {
                     $this->replyToken = $event['replyToken'];   // set replyToken
-                    $pesanMasuk = $event['message']['text'];
-
-                    // menentukan kategori pesan masuk
-                    $this->salamModel->setConfidence($pesanMasuk);
-                    $this->terimaKasihModel->setConfidence($pesanMasuk);
-
                     $this->salam();
                 }
             }
