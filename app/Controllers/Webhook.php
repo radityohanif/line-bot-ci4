98dd7537c08d8122c3ab5f7a7d923f13521e58d9
radityohanif <sayaanakit80@gmail.com>
2022-01-01 17:06:03 +0700

    latihan chatbot

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -7,7 +7,6 @@ use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use LINE\LINEBot\SignatureValidator as SignatureValidator;
 
 class Webhook extends BaseController
 {
@@ -22,10 +21,8 @@ class Webhook extends BaseController
     public function __construct()
     {
         // Sesuaikan dengan identitas chatbot
-        $this->channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
-        $this->channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
-
-        $this->pass_signature = false;
+        $this->channel_access_token = "NbrmweDZ9dDYHaePjMsH4QaPyuNQXRTmdl0drAn112Tmurwl35ibzKCPWmZKZIkQSdvShINECInTnsyHUn7jQ4Rqbn2PC7YOR0oMiRZHwZ+IP3F1SI4FjEw1Q7ZpKs3RyB4ZOE8UGdtHrPVsmHvNBQdB04t89/1O/w1cDnyilFU=";
+        $this->channel_secret = "07c25222ac6b0b0499f07127aac90e30";
         $this->httpClient = new CurlHTTPClient($this->channel_access_token);
         $this->bot = new LINEBot($this->httpClient, ['channelSecret' => $this->channel_secret]);
     }
@@ -34,23 +31,9 @@ class Webhook extends BaseController
     {
         // get request body and line signature header
         $body = $this->request->getBody();
-        $signature = $this->request->getHeaderLine("HTTP_X_LINE_SIGNATURE");
 
         // log body and signature
         file_put_contents('php://stderr', 'Body: ' . $body);
-
-        if (!$this->pass_signature) {
-
-            // if LINE_SIGNATURE exist in request header
-            if (empty($signature)) {
-                return "Signature does not exist, request fail";
-            }
-
-            // is this request comes from LINE?
-            if (!SignatureValidator::validateSignature($body, $this->channel_secret, $signature)) {
-                return "Signature is invalid, not from LINE";
-            }
-        }
 
         // Chatbot code start from here.. 👇👇
         // Fetch request data
@@ -69,7 +52,7 @@ class Webhook extends BaseController
     {
         $result = $this->bot->replyText(
             $this->replyToken,
-            'Hello selamat datang di warung pecel lele seger roso :)'
+            'Hola 🙃'
         );
         $this->response->setContentType('application/json');
         $this->response->setStatusCode(200);
