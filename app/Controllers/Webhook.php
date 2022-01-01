56cf7464568183996d011a3365c2d0bdcb3a5bcc
radityohanif <sayaanakit80@gmail.com>
2022-01-01 16:54:48 +0700

    tes

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -6,44 +6,74 @@ use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\SignatureValidator as SignatureValidator;
 
 class Webhook extends BaseController
 {
+    protected $pass_signature;
+    protected $channel_access_token;
+    protected $channel_secret;
+    protected $httpClient;
+    protected $bot;
+    protected $request;
     protected $replyToken;
-    protected $LINEBot;
 
     public function __construct()
     {
-        // Initial LINEBot
-        $channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
-        $channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
-        $httpClient = new CurlHTTPClient($channel_access_token);
-        $this->LINEBot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+        // Sesuaikan dengan identitas chatbot
+        $this->channel_access_token = "ddHYmFx/7KWWmM4V58v/zHvicDqidb0Vp8kplVVq0uQjbUexytmu563i1WBhC4fNEB41b4NNIhrwwWtGLFykJMf5zrhO9wsKrZQUFNnjZtKiPp8OvKPh3RpQbcP09DqYR/nKuiSItxw1iBlk1b6GzQdB04t89/1O/w1cDnyilFU=";
+        $this->channel_secret = "f3dc9c53239aeab3dc0980c6b061cdac";
+
+        $this->pass_signature = false;
+        $this->httpClient = new CurlHTTPClient($this->channel_access_token);
+        $this->bot = new LINEBot($this->httpClient, ['channelSecret' => $this->channel_secret]);
     }
 
     public function index()
     {
-        // Get request body
+        // get request body and line signature header
         $body = $this->request->getBody();
+        $signature = $this->request->getHeaderLine("HTTP_X_LINE_SIGNATURE");
 
-        // Convert request data from json to array
-        $this->request = json_decode($body, true);
+        // log body and signature
+        file_put_contents('php://stderr', 'Body: ' . $body);
 
+        if (!$this->pass_signature) {
+
+            // if LINE_SIGNATURE exist in request header
+            if (empty($signature)) {
+                return "Signature does not exist, request fail";
+            }
+
+            // is this request comes from LINE?
+            if (!SignatureValidator::validateSignature($body, $this->channel_secret, $signature)) {
+                return "Signature is invalid, not from LINE";
+            }
+        }
+
+        // Chatbot code start from here.. 👇👇
         // Fetch request data
+        $this->request = json_decode($body, true);
         foreach ($this->request['events'] as $event) {
             if ($event['type'] == 'message') {
                 if ($event['message']['type'] == 'text') {
-                    // set replyToken
                     $this->replyToken = $event['replyToken'];
-                    // reply message
-                    $this->replyMessage();
+                    $this->hello();
                 }
             }
         }
     }
 
-    public function replyMessage()
+    public function hello()
     {
-        $this->LINEBot->replyText($this->replyToken, "Hello 🙂");
+        $result = $this->bot->replyText(
+            $this->replyToken,
+            'Hello selamat datang di warung pecel lele seger roso :)'
+        );
+        $this->response->setContentType('application/json');
+        $this->response->setStatusCode(200);
+        $this->response->setJSON($this->request->getBody()->write(json_encode($result->getJSONDecodedBody())));
+        return $this->response;
     }
 }
