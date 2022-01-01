ba5f16fad85b1bfb67cbf12f2e1fa21c906be17d
radityohanif <sayaanakit80@gmail.com>
2022-01-01 17:30:19 +0700

    still cant use model to give respone :(

--- app/Controllers/Webhook.php
+++ app/Controllers/Webhook.php
@@ -4,7 +4,6 @@ namespace App\Controllers;
 
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use App\Model\Greeting;
 use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
@@ -13,7 +12,6 @@ class Webhook extends BaseController
 {
     protected $replyToken;
     protected $bot;
-    protected $model;
 
     public function __construct()
     {
@@ -22,9 +20,6 @@ class Webhook extends BaseController
         $channel_secret = "07c25222ac6b0b0499f07127aac90e30";
         $httpClient = new CurlHTTPClient($channel_access_token);
         $this->bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
-
-        // initialize external model
-        $this->model = new Greeting();
     }
 
     public function index()
@@ -50,10 +45,9 @@ class Webhook extends BaseController
 
     public function hello()
     {
-        $replyMessage = $this->model->getReplyMessage();
         $this->bot->replyText(
             $this->replyToken,
-            $replyMessage
+            "Hola from webhook controller"
         );
     }
 }
